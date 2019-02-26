2640417da18c2af5b2b5494db330ad5dba748ee6
raulrp3 <raul.ramirez@escuelaestech.es>
2019-02-26 17:50:11 +0100

    add new validations

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-	hideAlert();
+	hideAlerts();
 	let textId = document.getElementById("textId");
 	textId.addEventListener("blur",function(){validateId()});
 	let textPassword = document.getElementById("textPassword");
@@ -14,7 +14,7 @@ window.onload = function(){
 	btnSave.addEventListener("click",function(){submitForm()});
 }
 
-function hideAlert(){
+function hideAlerts(){
 	let divAlert = document.getElementsByClassName("alert");
 	for (let i = 0;i < divAlert.length;i++){
 		divAlert[i].style.display = "none";
@@ -25,7 +25,12 @@ function validateId(){
 	let alertId = document.getElementById("alertId");
 	let id = event.target.value;
 	if (!validateEmpty(alertId,id)){
-		event.target.classList.add("border-danger");
+		addClass(event.target,"border-danger");
+	}else if(!validateLength(5,12,id,alertId)){
+		addClass(event.target,"border-danger");
+	}else{
+		hideAlert(alertId);
+		removeClass(event.target,"border-danger");
 	}
 }
 
@@ -33,46 +38,80 @@ function validateEmpty(alertError,value){
 	const MESSAGE = "Campo obligatorio"
 	let isValid = true;
 	if (!value){
-		alertError.style.display = "block";
-		alertError.innerHTML = MESSAGE;
+		showAlert(alertError,MESSAGE);
 		isValid = false;
 	}
+	return isValid;
 }
 
 function validatePassword(){
 	let alertPassword = document.getElementById("alertPassword");
 	let password = event.target.value;
 	if (!validateEmpty(alertPassword,password)){
-		event.target.classList.add("border-danger");
+		addClass(event.target,"border-danger");
+	}else if(!validateLength(7,12,password,alertPassword)){
+		addClass(event.target,"border-danger");
+	}else{
+		hideAlert(alertPassword);
+		removeClass(event.target,"border-danger");
 	}
 }
 
 function validateName(){
 	let alertName = document.getElementById("alertName");
 	let name = event.target.value;
+	let expReg = new RegExp(/^[A-Za-z\s]+$/g);
+	const MESSAGE = "El nombre no puede contener números";
 	if (!validateEmpty(alertName,name)){
-		event.target.classList.add("border-danger");
+		addClass(event.target,"border-danger");
+	}else if(!validateValue(name,expReg,alertName,MESSAGE)){
+		addClass(event.target,"border-danger");
+	}else{
+		hideAlert(alertName);
+		removeClass(event.target,"border-danger");
 	}
 }
 
 function validateEmail(){
 	let alertEmail = document.getElementById("alertEmail");
 	let email = event.target.value;
+	let regExp = new RegExp(/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i);
+	const MESSAGE = "Email no válido";
 	if (!validateEmpty(alertEmail,email)){
-		event.target.classList.add("border-danger");
+		addClass(event.target,"border-danger");
+	}else if(!validateValue(email,regExp,alertEmail,MESSAGE)){
+		addClass(event.target,"border-danger");
+	}else{
+		hideAlert(alertEmail);
+		removeClass(event.target,"border-danger");
 	}
 }
 
 function validateZip(){
 	let alertZip = document.getElementById("alertZip");
 	let zip = event.target.value;
+	let expReg = new RegExp(/^[0-9\s]+$/g);
+	const MESSAGE = "El código zip no puede contener letras";
 	if (!validateEmpty(alertZip,zip)){
-		event.target.classList.add("border-danger");
+		addClass(event.target,"border-danger");
+	}else if(!validateValue(zip,expReg,alertZip,MESSAGE)){
+		addClass(event.target,"border-danger");
+	}else{
+		hideAlert(alertZip);
+		removeClass(event.target,"border-danger");
 	}
 }
 function submitForm(){
-	validateCountry();
-	validateLanguage();
+	let selectCountry = document.getElementById("selectCountry");
+	let alertCountry = document.getElementById("alertCountry");
+	let alertLanguage = document.getElementById("alertLanguage");
+	if (validateCountry()){
+		hideAlert(alertCountry);
+		removeClass(selectCountry,"border-danger");
+	}
+	if (validateLanguage()){
+		hideAlert(alertLanguage);
+	}
 }
 
 function validateCountry(){
@@ -81,23 +120,63 @@ function validateCountry(){
 	let alertCountry = document.getElementById("alertCountry");
 	let isValid = true;
 	if (countrySelected == "Selecciona un país..."){
-		selectCountry.classList.add("border-danger");
-		alertCountry.style.display = "block";
-		alertCountry.innerHTML = "Debes seleccionar un país";
+		addClass(selectCountry,"border-danger");
+		showAlert(alertCountry,"Debes seleccionar un país");
+		isValid = false;
 	}
+	return isValid;
 }
 
 function validateLanguage(){
 	let alertLanguage = document.getElementById("alertLanguage");
 	let checkLanguage = document.querySelectorAll("input[type = 'checkbox']");
 	let counter = 0;
+	let isValid = true;
 	for (let i = 0;i < checkLanguage.length;i++){
 		if (checkLanguage[i].checked){
 			counter++;
 		}
 	}
 	if (counter < 1){
-		alertLanguage.style.display = "block";
-		alertLanguage.innerHTML = "Debes seleccionar al menos un idioma";
+		showAlert(alertLanguage,"Debes seleccionar al menos un idioma")
+		isValid = false;
 	}
+	return isValid;
 }
+
+function validateLength(minLength,maxLength,value,alertError){
+	const MESSAGE = "Tiene que tener una longitud entre " + minLength + " y " + maxLength;
+	let isValid = true;
+	if (value.length < minLength || value.length > maxLength){
+		showAlert(alertError,MESSAGE);
+		isValid = false;
+	}
+	return isValid;
+}
+
+function validateValue(value,expReg,alertError,message){
+	let isValid = true;
+	if (!expReg.test(value)){
+		showAlert(alertError,message);
+		isValid = false;
+	}
+	return isValid;
+}
+
+function addClass(input,value){
+	input.classList.add(value);
+}
+
+function removeClass(input,value){
+	input.classList.remove(value);
+}
+
+function hideAlert(alertError){
+	alertError.style.display = "none";
+}
+
+function showAlert(alertError,message){
+	alertError.style.display = "block";
+	alertError.innerHTML = message;
+}
+
